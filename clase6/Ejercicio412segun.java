892820c45c440c46f623158c3f12786cbb5fe8e9
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-03-09 22:46:27 +0000

    Ejercicio412sg.java ok

---
+++ clase6/Ejercicio412segun.java
@@ -0,0 +1,29 @@
+package clase6;
+
+import java.util.Scanner;
+
+public class Ejercicio412segun {
+    public static void main(String[] args) {
+        
+     Scanner sc = new Scanner(System.in);
+     double num1,num2;
+     int seleccion;
+
+     System.out.println("Digite numnero 1");
+     num1 = sc.nextDouble();
+     System.out.println("Digite numero 2");
+     num2= sc.nextDouble();
+     System.out.println("Digite 1,2 o 3");
+     seleccion=sc.nextInt();
+
+     sc.close();
+
+    
+    
+    
+    
+    
+    
+    }
+    
+}
