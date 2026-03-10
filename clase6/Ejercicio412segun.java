79c957c0430de2445b5beed9f0869dd84b911c5c
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-03-10 00:13:08 +0000

    Ejercicio412segun.java ok

--- clase6/Ejercicio412segun.java
+++ clase6/Ejercicio412segun.java
@@ -1,40 +1,34 @@
 package clase6;
 
-public class Ejercicio412segun {
+import java.util.Scanner;
 
+public class Ejercicio412segun {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num1, num2;
+        int seleccion;
+
+        System.out.println("Digite numero 1");
         num1 = sc.nextDouble();
-        System.out.println("Digite número 2");
-        num2= sc.nextDouble();
-        System.out.println("Digite la seleccion 1, 2 o 3 ");
+        System.out.println("Digite numero 2");
+        num2 = sc.nextDouble();
+        System.out.println("Digite seleccion 1,2 o 3");
         seleccion = sc.nextInt();
 
-        switch (seleccion) {
-            case 1:
-                System.out.println("La suma es "+ (num1+num2));
-            
-                
-                break;
         
-            case 2 :
-                System.out.println("la multiplicacion es"+(num1*num2));
-                break;
-
-                case 3 : 
-                System.out.println("La division es;"+(num1/num2));
-                break,
 
 
+    }
 
+    
+    
+    {
+    
+        sc.close();
 
-
-            default:
-                System.out.println("opcion no valida");
-                break;
         
-            sc.close();
-           
-            }
+
     }
     
 }
