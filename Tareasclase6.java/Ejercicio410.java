e2ca442c6584d285a4108814b8f12a782c6d274e
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-03-15 04:19:14 +0000

    Ejercicio410switch.java ok

---
+++ Tareasclase6.java/Ejercicio410.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class Ejercicio410 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Ingrese un numero: ");
+        numero = sc.nextInt();
+
+        int tipo;
+
+        if (numero > 0) {
+            tipo = 1;   // positivo
+        } else if (numero < 0) {
+            tipo = -1;  // negativo
+        } else {
+            tipo = 0;   // cero
+        }
+
+        switch (tipo) {
+            case 1:
+                System.out.println("El numero es positivo");
+                break;
+
+            case -1:
+                System.out.println("El numero es negativo");
+                break;
+
+            case 0:
+                System.out.println("El numero es cero");
+                break;
+        }
+
+        sc.close();
+    }
+}
