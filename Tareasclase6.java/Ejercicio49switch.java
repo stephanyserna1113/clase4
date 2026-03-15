1d36e3c12920b0bddaddf6f32f43744aea79c9dc
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-03-15 04:13:32 +0000

    Ejercicio49switch.java ok

--- Tareasclase6.java/Ejercicio49switch.java
+++ Tareasclase6.java/Ejercicio49switch.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Ejercicio49 {
+public class Ejercicio49switch {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
