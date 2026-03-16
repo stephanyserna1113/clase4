ea51facfc02218b78824d74217fb0f1d168f7e04
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-03-16 16:12:31 +0000

    Ejercicio1.java ok

---
+++ Clase7/Ejercicio2.java
@@ -0,0 +1,12 @@
+package Clase7;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+
+        //ciclo for con incremento de 2 en 2
+        
+        for(int i = 2;i <= 20; i = i + 2){
+            System.out.println(i);
+    }
+    }
+}
---
+++ Clase7/Ejercicio3.java
@@ -0,0 +1,11 @@
+package Clase7;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        for(int i =)
+        
+    }
+    
+}
