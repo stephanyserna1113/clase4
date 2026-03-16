4dfc16e96671329661ee420f612f85fbf2edf2e4
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-03-16 16:46:16 +0000

    Ejercicio55.java ok

---
+++ Clase7/Ejercicio55.java
@@ -0,0 +1,12 @@
+package Clase7;
+
+public class Ejercicio55 {
+    public static void main(String[] args) {
+        // ciclo for con inremento de 2 en 2
+
+         for(int i=2; i <=1000; i = i+2);
+         System.out.println("i");
+
+    }
+    
+}
