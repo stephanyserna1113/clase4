9ca6dc1567735fff81f08d5174eb2e03e8700401
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-03-16 16:26:41 +0000

    Ejercicio3.java ok

--- Clase7/Ejercicio3.java
+++ Clase7/Ejercicio3.java
@@ -3,8 +3,14 @@ package Clase7;
 public class Ejercicio3 {
 
     public static void main(String[] args) {
+       int suma=0;
 
-        for(int i =)
+        for(int i =1; i <=10; i++){
+            suma=suma+i;
+            System.out.println("Suma total:"+suma);
+
+            
+        }
         
     }
     
