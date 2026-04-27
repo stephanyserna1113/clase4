95c73a8f4a87e63281a27eacd71b9f5c8c9a7360
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-04-27 16:40:55 +0000

    ok

---
+++ clase12/operaciones.java
@@ -0,0 +1,73 @@
+
+public class OperacionesMatematicas {
+
+ //Atributos
+ private int a;
+
+ private int b;
+
+
+ //Constructor de la clase
+
+public OperacionesMatematicas (int a, int b){
+
+this.a = a;
+
+this.b = b;
+
+ }
+
+
+
+ //Metodo de sumar
+
+ public int sumar(int a, int breturn a + b;
+
+ }
+
+
+//Metodo de restar
+
+public int restar(int a, int b) return a - b; }
+
+
+
+ //Metodo de multiplicar
+
+ public int multiplicar(int a, int b){
+ return a * b }
+
+
+
+//Metodo de dividir
+
+public double dividir(int a, int b){
+
+double total; if(b == 0){
+ total = 0 }else{
+
+ total = a / b;
+
+} return total;
+
+ }
+
+
+ //Metodo de potencia
+ public int potencia (int a, int b){
+
+int p = 1;
+ for (int i = 1; i <= b; i++) {
+
+ p = p * a;}
+
+ return p;
+
+ }
+
+
+
+}
+
+
+
