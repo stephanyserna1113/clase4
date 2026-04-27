c144fcbf6a1af2154ee7931665c9f63569add260
stephanyserna1113 <sbarandicas@estudiante.uniajc.edu.co>
2026-04-27 15:29:20 +0000

    Ejercicio.java ok

---
+++ semana12/Ejercicio.java
@@ -0,0 +1,46 @@
+package semana12;public class Estudiante {
+    
+    //Atributos
+    private int identificacion;
+    private String nombres;
+    private String apellidos;
+    private int edad;
+
+    //Constructor de la clase
+    //El constructor permite inicializar la clase
+    //El constructor se llama igual que la clase
+    //la palabra reservada "this" es una autoreferencia al atributo de la clase
+
+    public Estudiante(int identificacion, String nombres, String apellidos, int edad){
+        this.identificacion = identificacion;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    //Métodos get y set para el atributo nombres
+    public String getNombres(){
+        return nombres;
+    }
+
+    public void setNombres(String nombres){
+        this.nombres = nombres;
+    }
+
+    //El método toString permite mostrar la información que tiene el objeto.
+    public String toString(){
+        return "Estudiante { identificación: " + identificacion + 
+               " nombres y apellidos: " + nombres + " " + apellidos + 
+               " edad: " + edad + "}";
+    }
+
+    public double calcularNotaFinal (double n1, double n2, double n3){
+        double notaFinal = ((n1 * 0.30) + (n2 * 0.30) + (n3 * 0.40));
+        return notaFinal;
+    }
+
+}
+
+public class Ejercicio {
+    
+}
