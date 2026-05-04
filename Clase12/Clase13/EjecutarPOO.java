2a0303554db9b8c0036e766e983c3c1de7f30746
stephany serna <sbarandicas@estudiante.uniajc.edu.co>
2026-05-04 16:37:40 +0000

    ejercicio 29 ok

--- Clase12/Clase13/EjecutarPOO.java
+++ Clase12/Clase13/EjecutarPOO.java
@@ -1,11 +1,11 @@
 public class EjecutarPOO {
     public static void main(String[] args) {
-        
-        //Ejecución del ejercicio #23
-        //Instanciar
+
+        // Ejecución del ejercicio #23
+        // Instanciar
         Libro objLibro1 = new Libro("Tom Sawyer", "Mark Twain", 456);
         Libro objLibro2 = new Libro("100 Años de Soledad", "Gabriel García Márquez", 298);
-        //Mostrar los objetos
+        // Mostrar los objetos
         System.out.println(objLibro1);
         System.out.println(objLibro2);
     }
